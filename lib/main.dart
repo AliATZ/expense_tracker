2f81992ae9199f991529c26426f3287d301023f2
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-08-25 19:51:18 +0330

    chore(app): [02] Add Startup Codes And Placeholder Texts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:expense_tracker/expense.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(
+      const MaterialApp(
+          home: Expenses()
+      )
+  );
 }
