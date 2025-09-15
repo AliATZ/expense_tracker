883e74ad62c21ba863c129aa1144d648d0e2a8c7
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-09-15 21:43:39 +0330

    Add(app): [5-14] Replace Manual Controller With Built in Function

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -11,10 +11,12 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle= '';
+  final _titleController = TextEditingController();
 
-  void _saveTitleInput(String inputValue){
-    _enteredTitle = inputValue;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -24,12 +26,12 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(label: Text('Title')),
           ),
           Row(children: [
-            ElevatedButton(onPressed: (){print(_enteredTitle);}, child: Text('save Expense'))
+            ElevatedButton(onPressed: (){print(_titleController.text);}, child: Text('save Expense'))
           ],)
         ],
       ),
