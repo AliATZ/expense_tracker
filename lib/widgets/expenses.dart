f0e700a9c867cb6f3448526f565c86677a0f405b
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-09-19 13:25:24 +0330

    feat(app): [5-26] Add Undo Message for Removed Expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -43,14 +43,37 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
-  void _removeExpense(Expense expense){
+  void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense Deleted'),
+        action: SnackBarAction(label: 'Undo', onPressed: (){
+          setState(() {
+            _registeredExpenses.insert(expenseIndex, expense);
+          });
+        }),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = Center(child: Text('No Expenses Found'));
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Expense Tracker'),
@@ -62,7 +85,7 @@ class _ExpensesState extends State<Expenses> {
         child: Column(
           children: [
             Text('Placeholder'),
-            Expanded(child: ExpensesList(expenses: _registeredExpenses,onRemoveExpense: _removeExpense,)),
+            Expanded(child: mainContent),
           ],
         ),
       ),
