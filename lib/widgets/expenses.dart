d540b65d61f556934875fc1d96d032eab005b979
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-09-19 13:12:11 +0330

    feat(app): [5-25] Add Swipe Function for Dismissing

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -43,6 +43,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense){
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,7 +62,7 @@ class _ExpensesState extends State<Expenses> {
         child: Column(
           children: [
             Text('Placeholder'),
-            Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses,onRemoveExpense: _removeExpense,)),
           ],
         ),
       ),
