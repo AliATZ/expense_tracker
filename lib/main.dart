9843eefa52fe55002908d2727bf9346e7848d1e7
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-09-20 20:34:36 +0330

    feat(app): [5-29] Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,22 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
       ),
       home: Expenses(),
     ),
