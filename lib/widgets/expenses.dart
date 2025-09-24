c91953cc3eb461369abfc5033ab4cf7a0cc912db
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-09-24 21:25:51 +0330

    feat(app): [6-04] Add Landscape Mode

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -66,6 +66,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = Center(child: Text('No Expenses Found'));
 
     if (_registeredExpenses.isNotEmpty) {
@@ -83,12 +84,15 @@ class _ExpensesState extends State<Expenses> {
         ],
       ),
       body: Center(
-        child: Column(
+        child: width<600 ? Column(
           children: [
             Chart(expenses: _registeredExpenses,),
             Expanded(child: mainContent),
           ],
-        ),
+        ): Row(children:[
+          Expanded(child: Chart(expenses: _registeredExpenses,)),
+          Expanded(child: mainContent),
+        ]),
       ),
     );
   }
